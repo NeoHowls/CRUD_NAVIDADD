4f4c6fcb00114cdecfab69ec804bbe47d05713a8
FmartinezS21 <felipemartinez1971@gmail.com>
2024-06-19 14:42:47 -0400

    15:00

---
+++ model/model_Perfil.php
@@ -0,0 +1,43 @@
+
+<?php
+    //Solicito el archivo ConexionDB.php para conectarme a la base de dato
+    require_once '../config/ConexionDB.php';
+
+    //La clase Menu Model hereda funciones de ConexionDB
+    class MenuModel extends ConexionBD{
+
+        public function listar($CONSULTA){
+            //$sql = "SELECT * FROM A_ETNIA"
+            //Realizo la conexion paara comunicarme con la bdd
+            $this->connect();
+            //configuro la consulta 
+            $query = $this->ejecutaConsulta($CONSULTA);
+            //retorno la consulta hacia controller.php
+            return $query;
+        }
+
+        public function ejecutar($CONSULTA){
+            //$sql = "SELECT * FROM A_ETNIA";
+            //Realizo la conexion paara comunicarme con la bdd
+            $this->connect();
+            //configuro la consulta 
+            $query = $this->ejecutarOrden($CONSULTA);
+            //retorno la consulta hacia controller.php
+            return $query;
+        }
+        
+        //NO SE USARA AUN
+        public function mostrarPerfil(){
+   
+            $sql ="SELECT id, perfil FROM A_PERFIL";
+            $this->connect();
+            $query = $this->iniciar($sql);
+            
+                return $query;
+           
+        }
+
+    }
+
+
+?>
