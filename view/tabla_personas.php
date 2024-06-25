98ecb46894e1693c8bbebf06a131cbc63c867cee
FmartinezS21 <felipemartinez1971@gmail.com>
2024-06-25 10:14:20 -0400

    10:14(25-06)

--- view/tabla_personas.php
+++ view/tabla_personas.php
@@ -172,8 +172,7 @@
           </table>
           <script type="text/javascript" src="../datatables.js"></script>
           <script type="text/javascript" src="../js/idioma.js"></script>
-          <script type="text/javascript" src="../js/AJAX_PERSONA.js"></script> 
-          <script type="text/javascript" src="nacionalidad.js"></script>    
+          <script type="text/javascript" src="../js/AJAX_PERSONA.js"></script>    
     </div>
   </div>
 </div>
