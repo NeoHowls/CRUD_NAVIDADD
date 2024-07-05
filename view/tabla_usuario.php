b9a3f3cdada5a3492c7f83c8d9ad6f30484f3bed
NeoHowls <thealfafinal@gmail.com>
2024-07-05 12:32:16 -0400

    Editar_funciona_otravez

--- view/tabla_usuario.php
+++ view/tabla_usuario.php
@@ -2,7 +2,7 @@
       <!--Modal para CRUD-->
   <div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
-          <div class="modal-content" style="width: 110%;">
+          <div class="modal-content" style="width: 100%;">
               <div class="modal-header">
                   <h5 class="modal-title" id="exampleModalLabel"></h5>
   
